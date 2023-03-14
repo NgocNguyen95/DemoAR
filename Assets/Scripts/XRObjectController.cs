efda6e70ced191912a3ef1378953be6f70bead5c
NgocNguyen95 <31845250+NgocNguyen95@users.noreply.github.com>
2023-03-14 11:18:46 +0200

    Removed MRTK

--- Assets/Scripts/XRObjectController.cs
+++ Assets/Scripts/XRObjectController.cs
@@ -1,62 +1,23 @@
-using Microsoft.MixedReality.Toolkit.UI.BoundsControl;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Microsoft.MixedReality.Toolkit.UI.BoundsControlTypes;
-using Microsoft.MixedReality.Toolkit.UI;
 using System.Linq;
 
 namespace DemoAR
 {
     public class XRObjectController : MonoBehaviour
     {
-        BoundsControl _boundsControl;
-        ObjectManipulator _objectManipulator;
-
         private int _refSize = 5;
 
 
         private void Start()
         {
-            AddBoundsControl();
-            AddObjectManipulator();
-        }
 
-
-        private void AddBoundsControl()
-        {
-            _boundsControl = gameObject.AddComponent<BoundsControl>();
-
-            ConfigBoundsControl();
-
-            OptimizeObjectSize();
-        }
-
-
-        private void ConfigBoundsControl()
-        {
-            _boundsControl.BoundsControlActivation = BoundsControlActivationType.ActivateByPointer;
-        }
-
-
-        private void AddObjectManipulator()
-        {
-            _objectManipulator = gameObject.AddComponent<ObjectManipulator>();
-
-            ConfigObjectManipulator();
-        }
-
-
-        private void ConfigObjectManipulator()
-        {
-            _objectManipulator.OnManipulationStarted.AddListener((med) => _boundsControl.HighlightWires());
-            _objectManipulator.OnManipulationEnded.AddListener((med) => _boundsControl.UnhighlightWires());
         }
 
 
         private void OptimizeObjectSize()
         {
-            var objectSize = _boundsControl.TargetBounds.size;
+            var objectSize = new Vector3();
 
             Debug.Log($"[{nameof(XRObjectController)}] {nameof(OptimizeObjectSize)} object size: {objectSize}");
 
@@ -69,7 +30,6 @@ namespace DemoAR
             var updatedScale = 1 / (maxSize / _refSize);
 
             gameObject.transform.GetChild(0).transform.localScale = new Vector3(updatedScale, updatedScale, updatedScale);
-            _boundsControl.UpdateBounds();
 
             UpdatePosition();
         }
